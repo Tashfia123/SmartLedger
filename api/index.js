fa8244f032dc73736ff1e9ad1a38a7590ff6dbe7
Tashfia123 <tashfiahassan@iut-dhaka.edu>
2025-11-18 13:11:37 +0600

    index updated

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 // Vercel serverless function wrapper for Express API
-const serverlessExpress = require("@vendia/serverless-express");
+// const serverlessExpress = require("@vendia/serverless-express");
+// const app = require("../index.js");
+
+// // Wrap Express app for Vercel serverless
+// const handler = serverlessExpress({ app });
+
+// module.exports = handler;
+//
+
+// api/index.js
 const app = require("../index.js");
 
-// Wrap Express app for Vercel serverless
-const handler = serverlessExpress({ app });
-
-module.exports = handler;
+// Export the Express app directly – Vercel handles the serverless bridge
+module.exports = app;
